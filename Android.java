a024279fd6f90a507c29c71273720c6710a3c48a
1nn0c3ntu5 <100656593+1nn0c3ntu5@users.noreply.github.com>
2022-06-03 23:30:14 +0700

    Add files via upload

---
+++ Android.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kuis;
+
+/**
+ *
+ * @author WXO
+ */
+public class Android extends Test implements PembobotanNilai{
+    final double pTulis = 0.2, pCoding=0.5, pWawancara=0.3;
+
+    public Android(String NIK, String nama, int tesTulis, int tesCoding, int tesWawancara) {
+        super(tesTulis, tesCoding, tesWawancara, NIK, nama);
+    }    
+    
+    @Override
+    public void getKelulusan() {
+        double nilaiAkhir = (getTesTulis()*pTulis) + (getTesCoding()*pCoding) + (getTesWawancara()*pWawancara);
+        System.out.println("Nilai akhir : " + nilaiAkhir);
+        if(nilaiAkhir >= nilaiLulus){
+            System.out.println("Keterangan : LULUS");
+            System.out.println("Selamat kepada " + nama + " telah diterima sebagai Android");
+        }else{
+            System.out.println("Keterangan : GAGAL");
+            System.out.println("Mohon maaf kepada " + nama + " telah ditolak sebagai Android");
+        } 
+    }
+    
+    
+}
---
+++ Web.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kuis;
+
+/**
+ *
+ * @author WXO
+ */
+public class Web extends Test implements PembobotanNilai{
+    final double pTulis = 0.4, pCoding=0.35, pWawancara=0.25;
+
+    public Web(String NIK, String nama, int tesTulis, int tesCoding, int tesWawancara) {
+        super(tesTulis, tesCoding, tesWawancara, NIK, nama);
+    }    
+    
+    @Override
+    public void getKelulusan() {
+        double nilaiAkhir = (getTesTulis()*pTulis) + (getTesCoding()*pCoding) + (getTesWawancara()*pWawancara);
+        System.out.println("Nilai akhir : " + nilaiAkhir);
+        if(nilaiAkhir >= nilaiLulus){
+            System.out.println("Keterangan : LULUS");
+            System.out.println("Selamat kepada " + nama + " telah diterima sebagai Web");
+        }else{
+            System.out.println("Keterangan : GAGAL");
+            System.out.println("Mohon maaf kepada " + nama + " telah ditolak sebagai Web");
+        } 
+    }
+}
